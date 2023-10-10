8021a6f94050980dd9789dd17357b3455444e185
nunea39 <andryn1217@gmail.com>
2023-10-10 19:47:36 -0400

    First commit

---
+++ src/main/java/com/mycompany/tipcalculator/App.java
@@ -0,0 +1,32 @@
+package com.mycompany.tipcalculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+   public void start(Stage stage) throws Exception {
+      Parent root = 
+         FXMLLoader.load(getClass().getResource("primary.fxml"));
+
+      Scene scene = new Scene(root); // attach scene graph to scene
+      stage.setTitle("Tip Calculator"); // displayed in window's title bar
+      stage.setScene(scene); // attach scene to stage
+      stage.show(); // display the stage
+   }
+
+   public static void main(String[] args) {
+      // create a TipCalculator object and call its start method
+      launch(args); 
+   }
+
+}
